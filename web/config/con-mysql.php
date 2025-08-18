cd0423386d4c0b3d7b7ea3d69dcb2783516eec8d
Gustavo Luiz Gordoni <gustavogordoni1@gmail.com>
2025-08-18 20:03:42 -0300

    enabling access to the database through environment variables

--- web/config/con-mysql.php
+++ web/config/con-mysql.php
@@ -5,6 +5,14 @@ $user = 'root';
 $pass = 'root';
 $db = 'teste'; // Mude para o nome do seu banco
 
+if (file_exists(__DIR__ . '/../.env')) {
+    $env = parse_ini_file(__DIR__ . '/../.env', false, INI_SCANNER_RAW);
+    $server = $env['MYSQL_CONTAINER_NAME'] ?? $server;
+    $user   = 'root';
+    $pass   = $env['MYSQL_ROOT_PASSWORD'] ?? $pass;
+    $db     = $env['MYSQL_DATABASE'] ?? $db;
+}
+
 try {
     $pdo = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $user, $pass);
 } catch (PDOException $err) {
--- web/config/con-pgsql.php
+++ web/config/con-pgsql.php
@@ -7,6 +7,14 @@ $db = 'postgres';
 $user = 'postgres';
 $pass = 'postdba';
 
+if (file_exists(__DIR__ . '/../.env')) {
+    $env = parse_ini_file(__DIR__ . '/../.env', false, INI_SCANNER_RAW);
+    $server = $env['POSTGRES_CONTAINER_NAME'] ?? $server;
+    $db     = $env['POSTGRES_DB'] ?? $db;
+    $user   = $env['POSTGRES_USER'] ?? $user;
+    $pass   = $env['POSTGRES_PASSWORD'] ?? $pass;
+}
+
 try {
     $pdo = new PDO("pgsql:host=$server;dbname=$db;", $user, $pass);
 } catch (PDOException $err) {
